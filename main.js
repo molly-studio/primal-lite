605813f2500b66aef1393b3be4f8a79c28df905b
jaytel0 <165323192+jaytel0@users.noreply.github.com>
2025-02-08 14:05:21 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+// Main entry point - loads all components
+document.write('<script src="components/view.js"></script>');
+document.write('<script src="components/news.js"></script>');
+document.write('<script src="components/stories.js"></script>');
+document.write('<script src="components/carousel.js"></script>');
