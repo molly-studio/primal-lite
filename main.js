a0af7ff059249bf796483b0f26aa6f614075ebcf
Jaytel <165323192+jaytel0@users.noreply.github.com>
2025-02-08 14:13:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ import './views.js';
 import './news.js';
 import './stories.js';
 import './carousel.js';
+
+// Erase Hidden CMS Items from DOM Begin //
+    const eraseHidden =  () => {
+      document
+      .querySelectorAll(".w-condition-invisible")
+      .forEach(
+        el => {
+          el.remove();
+        }
+      );
+  }
+  document.addEventListener("DOMContentLoaded", eraseHidden);
