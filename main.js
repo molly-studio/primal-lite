bc39437b26601e3a5dd893527f8145af33c0a738
jaytel0 <165323192+jaytel0@users.noreply.github.com>
2025-02-08 14:08:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-// Main entry point - loads all components
-document.write('<script src="components/view.js"></script>');
-document.write('<script src="components/news.js"></script>');
-document.write('<script src="components/stories.js"></script>');
-document.write('<script src="components/carousel.js"></script>');
+console.log("Primal: Main.js loaded");
+
+// Your actual component files
+import './views.js';
+import './news.js';
+import './stories.js';
+import './carousel.js';
